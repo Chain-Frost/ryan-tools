4fcf28b1f2fbd65abeadeb33f77dcd9769b057ed
Chain Frost <chainfrost@outlook.com>
2024-10-27 20:13:49 +0800

    asc2asc bat

---
+++ ryan-scripts/TUFLOW-bat/asc2asc_max_by_search.bat
@@ -0,0 +1,5 @@
+set asc2asc=C:\TUFLOW\asc_to_asc.2024-06-AB\asc_to_asc_w64.exe
+%asc2asc% -b -out site_002_EXG_PMP_16M_d_HR_Max_MaxMax.tif -max site_002_EXG_PMP_*_16M_d_HR_Max.tif
+%asc2asc% -b -out site_002_EXG_PMP_16M_h_HR_Max_MaxMax.tif -max site_002_EXG_PMP_*_16M_h_HR_Max.tif
+%asc2asc% -b -out site_002_EXG_PMP_16M_V_Max_MaxMax.tif    -max site_002_EXG_PMP_*_16M_V_Max.tif
+Pause
