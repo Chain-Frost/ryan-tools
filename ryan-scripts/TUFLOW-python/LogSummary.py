c742a5efda5edf927c96c6f58366e74a1aa98a20
Chain Frost <chainfrost@outlook.com>
2025-01-21 09:29:46 +0800

    missed a commit?

--- ryan-scripts/TUFLOW-python/LogSummary.py
+++ ryan-scripts/TUFLOW-python/LogSummary.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     # Change working directory to the script's directory
     try:
         script_dir: Path = Path(__file__).resolve().parent
+
         # script_dir = Path(r"E:\Library\Automation\ryan-tools\tests\test_data")
         # script_dir = Path(
         #     r"E:\Library\Automation\ryan-tools\tests\test_data\tuflow\tutorials\Module_11"
