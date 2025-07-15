e9b88576786297f5710e7e640eee5da6d8300a11
Ryan Brook <ryan.brook@bge-resources.com>
2025-07-15 13:51:05 +0800

    package tuflow results styling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="ryan_functions",
     # Version scheme: yy.mm.dd.release_number
     # Increment when publishing new wheels
-    version="25.07.14.1",
+    version="25.07.15.1",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,  # Include package data as specified in MANIFEST.in
     # package_data={"ryan_library": ["py.typed"]},
