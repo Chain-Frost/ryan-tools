37b325bc5a913220ebff023616fcca5de111fa07
Chain-Frost <chainfrost@outlook.com>
2026-09-13 17:11:48 +0800

    Align alternative validation with lint policy

--- ryan_library/classes/culvert/alternative.py
+++ ryan_library/classes/culvert/alternative.py
@@ -15,5 +15,6 @@ class Alternative:
     def __post_init__(self) -> None:
         name = self.name.strip()
         if not name:
-            raise ValueError("name must be nonempty text.")
+            msg = "name must be nonempty text."
+            raise ValueError(msg)
         object.__setattr__(self, "name", name)
